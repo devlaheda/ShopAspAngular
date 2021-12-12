4160b4369b11153d25a474b9afe4418293388c46
Lahcen <lahcen.edaif@protonmail.com>
2021-12-12 17:02:31 +0100

    init

---
+++ Infrastructure/Data/ShopContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext(DbContextOptions<ShopContext> options) : base(options)
+        {
+        }
+        
+        public DbSet<Product> Products { get; set; }
+        
+    }
+}
